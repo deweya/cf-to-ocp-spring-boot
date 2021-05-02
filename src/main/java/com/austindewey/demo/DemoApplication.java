9df97037e1ddc105496903546cc5e0ebaae521f9
Austin Dewey <deweya964@gmail.com>
2021-05-02 05:07:34 -0500

    creating h2 profile and basic jpa functionality

--- src/main/java/com/austindewey/demo/DemoApplication.java
+++ src/main/java/com/austindewey/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.austindewey.demo;
 
+import com.austindewey.demo.model.Customer;
+import com.austindewey.demo.repository.CustomerRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+		};
 	}
 
 }
--- src/main/java/com/austindewey/demo/controller/MainController.java
+++ src/main/java/com/austindewey/demo/controller/MainController.java
@@ -1,12 +1,36 @@
 package com.austindewey.demo.controller;
 
+import java.util.List;
+
+import com.austindewey.demo.model.Customer;
+import com.austindewey.demo.repository.CustomerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
     @RequestMapping("/")
     public String index() {
         return "Hello World!";
+    }
+
+    @RequestMapping(value="/save", method = RequestMethod.POST)
+    public HttpStatus insertCustomer(@RequestBody Customer customer) {
+        boolean status = customerRepository.save(customer) != null;
+        return status ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST;
+    }
+
+    @RequestMapping("/findall")
+    public List<Customer> findAll() {
+        return (List<Customer>) customerRepository.findAll();
     }
 }
